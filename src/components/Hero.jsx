674fabf58fb8620776c487e4d49339ae7cb434dc
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-18 16:48:51 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,15 @@
-import { useRef, useEffect, useState } from "react";
-import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
 import Typewriter from "typewriter-effect";
 
 import { curve, heroBackground } from "../assets";
-import sample1 from "../assets/hero/sample1.jpg";
-import sample2 from "../assets/hero/sample2.jpg";
-
 import { heroIcons } from "../constants";
 import Button from "./Button";
 import CompanyLogos from "./CompanyLogos";
 import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
-import Generating from "./Generating";
-import Notification from "./Notification";
 import Section from "./Section";
-
-const images = [sample1, sample2];
 
 const Hero = () => {
   const parallaxRef = useRef(null);
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prev) => (prev + 1) % images.length);
-    }, 4000);
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <Section
@@ -56,8 +40,7 @@ const Hero = () => {
           </h1>
 
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unlock the next level of game scripting 
-            with{" "}
+            Unlock the next level of game scripting{" "}
             <span className="inline-block relative font-semibold">
               Qarvo
               <img
@@ -76,49 +59,19 @@ const Hero = () => {
           </Button>
         </div>
 
-        <div className="relative max-w-5xl mx-auto xl:mb-24">
-          {/* Removed conic-border wrapper */}
-          <div className="relative overflow-hidden rounded-[1.5rem]">
-            <img
-              src={images[currentSlide]}
-              alt={`Slide ${currentSlide + 1}`}
-              className="w-full object-cover h-auto rounded-[1.5rem]"
-            />
+        {/* Bagian gambar robot dihapus seluruhnya */}
 
-            <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
-
-            <ScrollParallax isAbsolutelyPositioned>
-              <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-transparent rounded-2xl xl:flex">
-                {heroIcons.map((icon, index) => (
-                  <li className="p-5" key={index}>
-                    <img src={icon} width={24} height={25} alt={icon} />
-                  </li>
-                ))}
-              </ul>
-            </ScrollParallax>
-
-            <ScrollParallax isAbsolutelyPositioned>
-              <Notification
-                className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
-                title="Code generation"
-              />
-            </ScrollParallax>
-          </div>
-
-          <Gradient />
-
-          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
-            <img
-              src={heroBackground}
-              className="w-full pointer-events-none select-none"
-              width={1440}
-              height={1800}
-              alt="Hero"
-            />
-          </div>
-
-          <BackgroundCircles />
+        <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+          <img
+            src={heroBackground}
+            className="w-full pointer-events-none select-none"
+            width={1440}
+            height={1800}
+            alt="Hero"
+          />
         </div>
+
+        <BackgroundCircles />
 
         <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
