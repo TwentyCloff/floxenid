d4b8025e93b42d66f15d9f319ea58ee8f703405c
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-18 15:03:51 +0700

    Update Benefits.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,4 +1,3 @@
-import Arrow from "../assets/svg/Arrow";
 import ClipPath from "../assets/svg/ClipPath";
 import { benefits } from "../constants";
 import { GradientLight } from "./design/Benefits";
@@ -14,9 +13,9 @@ const Benefits = () => {
           className="md:max-w-md lg:max-w-2xl"
           title={
             <>
-              Chat Smarter, Not Harder with{" "}
+              Why Choose{" "}
               <span className="inline-block relative font-semibold">
-                Brainwave
+                Qarvo?
                 <img
                   src={curve}
                   className="absolute top-full left-0 w-full xl:-mt-2 pointer-events-none select-none"
@@ -48,11 +47,6 @@ const Benefits = () => {
                     height={48}
                     alt={benefit.title}
                   />
-
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
-                    Explore more
-                  </p>
-                  <Arrow />
                 </div>
               </div>
 
