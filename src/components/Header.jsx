c49ed4f7b8532ce03eb820aa59fe5a2c3adebe5d
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-18 22:49:00 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,8 +32,8 @@ const Header = () => {
     <div
       className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
         openNavigation
-          ? "bg-[#0a0a0a] backdrop-blur-md"
-          : "bg-[#0a0a0a]/70 backdrop-blur-md"
+          ? "bg-black backdrop-blur-md"
+          : "bg-black/40 backdrop-blur-md"
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
@@ -47,7 +47,7 @@ const Header = () => {
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-[#0a0a0a] lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-black lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
