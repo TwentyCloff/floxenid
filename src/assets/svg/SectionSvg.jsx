5aab45be8243163562a446b6d1ad45ea982c39e9
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-18 20:00:16 +0700

    Update SectionSvg.jsx

--- src/assets/svg/SectionSvg.jsx
+++ src/assets/svg/SectionSvg.jsx
@@ -1,18 +1,28 @@
 import PlusSvg from "./PlusSvg";
 
 const SectionSvg = ({ crossesOffset }) => {
+  // crossesOffset contoh: "lg:translate-y-[5.25rem]"
+  // Kita akan ubah cara pakainya supaya mengubah posisi top
+
   return (
     <>
       <PlusSvg
-        className={`hidden absolute -top-[0.3125rem] left-[1.5625rem] ${
-          crossesOffset && crossesOffset
-        } pointer-events-none lg:block xl:left-[2.1875rem]`}
+        className={`hidden absolute left-[1.5625rem] pointer-events-none lg:block xl:left-[2.1875rem] ${
+          crossesOffset ? crossesOffset.replace("translate-y", "top") : "top-[1.25rem]"
+        }`}
+        style={{
+          // fallback kalau crossesOffset ga dikasih, default top sekitar 1.25rem
+          top: crossesOffset ? undefined : "1.25rem",
+        }}
       />
 
       <PlusSvg
-        className={`hidden absolute  -top-[0.3125rem] right-[1.5625rem] ${
-          crossesOffset && crossesOffset
-        } pointer-events-none lg:block xl:right-[2.1875rem]`}
+        className={`hidden absolute right-[1.5625rem] pointer-events-none lg:block xl:right-[2.1875rem] ${
+          crossesOffset ? crossesOffset.replace("translate-y", "top") : "top-[1.25rem]"
+        }`}
+        style={{
+          top: crossesOffset ? undefined : "1.25rem",
+        }}
       />
     </>
   );
