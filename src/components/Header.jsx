b37fe563fc5ad26b353c1cbe488287788b1b785c
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-19 19:23:14 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,64 +29,70 @@ const Header = () => {
   };
 
   return (
-    <div
-      className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
-        openNavigation
-          ? "bg-black backdrop-blur-md"
-          : "bg-black/20 backdrop-blur-md"
-      }`}
-      style={{ height: "68px" }}
-    >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 py-3 h-full">
-        <a
-          className="block w-auto xl:mr-8 text-3xl font-bold bg-gradient-to-r from-purple-500 to-indigo-500 text-transparent bg-clip-text"
-          href="#hero"
-        >
-          Qarvo
-        </a>
+    <>
+      {openNavigation && (
+        <div className="fixed inset-0 bg-black/80 z-40 lg:hidden transition-opacity duration-300" />
+      )}
 
-        <nav
-          className={`${
-            openNavigation ? "flex" : "hidden"
-          } fixed top-[68px] left-0 right-0 bottom-0 bg-black lg:static lg:flex lg:mx-auto lg:bg-transparent`}
-        >
-          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-            {navigation.map((item) => (
-              <a
-                key={item.id}
-                href={item.url}
-                target={item.external ? "_blank" : "_self"}
-                rel={item.external ? "noreferrer noopener" : undefined}
-                onClick={handleClick}
-                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                  item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:mr-0.25 lg:text-xs lg:font-semibold ${
-                  item.url === pathname.hash
-                    ? "z-2 lg:text-n-1"
-                    : "lg:text-n-1/50"
-                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
-              >
-                {item.title}
-              </a>
-            ))}
-          </div>
+      <div
+        className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
+          openNavigation
+            ? "bg-black backdrop-blur-md"
+            : "bg-black/20 backdrop-blur-md"
+        }`}
+        style={{ height: "68px" }}
+      >
+        <div className="flex items-center px-5 lg:px-7.5 xl:px-10 py-3 h-full">
+          <a
+            className="block w-auto xl:mr-8 text-3xl font-bold bg-gradient-to-r from-purple-500 to-indigo-500 text-transparent bg-clip-text"
+            href="#hero"
+          >
+            Qarvo
+          </a>
 
-          <HambugerMenu />
-        </nav>
+          <nav
+            className={`${
+              openNavigation ? "flex" : "hidden"
+            } fixed top-[68px] left-0 right-0 bottom-0 bg-black lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          >
+            <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+              {navigation.map((item) => (
+                <a
+                  key={item.id}
+                  href={item.url}
+                  target={item.external ? "_blank" : "_self"}
+                  rel={item.external ? "noreferrer noopener" : undefined}
+                  onClick={handleClick}
+                  className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                    item.onlyMobile ? "lg:hidden" : ""
+                  } px-6 py-6 md:py-8 lg:mr-0.25 lg:text-xs lg:font-semibold ${
+                    item.url === pathname.hash
+                      ? "z-2 lg:text-n-1"
+                      : "lg:text-n-1/50"
+                  } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                >
+                  {item.title}
+                </a>
+              ))}
+            </div>
 
-        <Button className="hidden lg:flex" href={links.sourceCode} external>
-          Source Code
-        </Button>
+            <HambugerMenu />
+          </nav>
 
-        <Button
-          onClick={toggleNavigation}
-          className="ml-auto lg:hidden"
-          px="px-3"
-        >
-          <MenuSvg openNavigation={openNavigation} />
-        </Button>
+          <Button className="hidden lg:flex" href={links.sourceCode} external>
+            Source Code
+          </Button>
+
+          <Button
+            onClick={toggleNavigation}
+            className="ml-auto lg:hidden"
+            px="px-3"
+          >
+            <MenuSvg openNavigation={openNavigation} />
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
