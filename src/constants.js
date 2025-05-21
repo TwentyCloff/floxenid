f3cd8e37a060c3b057a3735d421add40d01dbe3a
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-21 21:57:12 +0700

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+// constants.js
+export const pricing = [
+  {
+    id: "firebaseDocumentId1",  // Must match Firebase document ID
+    title: "Premium Plan",
+    price: 99,
+    premium: true,
+    features: [...]
+  },
+  {
+    id: "firebaseDocumentId2", 
+    title: "Free Plan",
+    price: 0,
+    premium: false,
+    features: [...]
+  }
+];
