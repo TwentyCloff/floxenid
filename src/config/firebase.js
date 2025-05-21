4cf1ab4a189c089e4271ae354d5d6036ae56e00b
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-21 18:34:13 +0700

    Create firebase.js

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+// src/config/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDP6XNlI1jUgHN3pVWIXNZjGT3YWXKSdes",
+  authDomain: "gweenlearn.firebaseapp.com",
+  projectId: "gweenlearn",
+  storageBucket: "gweenlearn.firebasestorage.app",
+  messagingSenderId: "915816429541",
+  appId: "1:915816429541:web:65c885efda4472930c210c",
+  measurementId: "G-RSGBWRE6BD"
+};
+
+// Initialize Firebase app once
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
