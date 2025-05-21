0f811c27c496cecaf8cee270b93f7852d8936cd9
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-21 07:43:33 +0700

    Update CustomCursor.jsx

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 
 // Import gambar planet
-import planet1 from "../asseth/planet-1.png";
-import planet2 from "../asseth/planet-2.png";
-import planet3 from "../asseth/planet-3.png";
-import planet4 from "../asseth/planet-4.png";
-import planet5 from "../asseth/planet-5.png";
+import planet1 from "../assets/planet-1.png";
+import planet2 from "../assets/planet-2.png";
+import planet3 from "../assets/planet-3.png";
+import planet4 from "../assets/planet-4.png";
+import planet5 from "../assets/planet-5.png";
 
 const planets = [planet1, planet2, planet3, planet4, planet5];
 
