604bc4e7cf67f9972fa107529a3f512377beff7c
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-21 18:35:46 +0700

    Update ProjectDetails.jsx

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,21 +1,9 @@
+// src/pages/ProjectDetails.jsx
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDP6XNlI1jUgHN3pVWIXNZjGT3YWXKSdes",
-  authDomain: "gweenlearn.firebaseapp.com",
-  projectId: "gweenlearn",
-  storageBucket: "gweenlearn.firebasestorage.app",
-  messagingSenderId: "915816429541",
-  appId: "1:915816429541:web:65c885efda4472930c210c",
-  measurementId: "G-RSGBWRE6BD"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 const ProjectDetails = () => {
   const { id } = useParams();
@@ -32,7 +20,7 @@ const ProjectDetails = () => {
         if (docSnap.exists()) {
           setProject(docSnap.data());
         } else {
-          // Jika tidak ada data, redirect ke home
+          // Jika project tidak ditemukan, kembali ke halaman utama
           navigate("/");
         }
       } catch (error) {
