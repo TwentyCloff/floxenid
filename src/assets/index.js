bd111da00bde3f5bcee21d326ddd655bf2f0b072
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-21 17:33:36 +0700

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,6 +21,7 @@ import chromecast from "./chrome-cast.svg";
 import sliders04 from "./sliders-04.svg";
 import loading from "./loading.png";
 import background from "./background.jpg";
+import verificon from "./verif8icon.png";
 
 import curve from "./hero/curve.png";
 import robot from "./hero/robot.jpg";
