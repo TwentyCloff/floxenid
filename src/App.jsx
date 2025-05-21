d5101fc0e92895b44aebe8503bb0a53c38954a5c
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-21 10:21:45 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,10 @@ import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
-import CustomCursor from "./components/CustomCursor"; // ✅ Tambahan
 
 const App = () => {
   return (
     <>
-      <CustomCursor /> {/* ✅ Custom cursor global */}
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
