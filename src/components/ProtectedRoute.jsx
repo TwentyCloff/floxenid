e5a31dbeb2cd8cfb680b2d7eebaced913ffaa786
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-22 16:27:35 +0700

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = localStorage.getItem("isAdminLoggedIn") === "true";
+  const isLoggedIn = localStorage.getItem("admin-auth") === "true";
 
   if (!isLoggedIn) {
     return <Navigate to="/admin/login" replace />;
