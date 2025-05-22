637b77498608b19eb0303dbe5663116f7c633a60
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-22 11:51:05 +0700

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,4 +1,4 @@
-// src/config/firebase.js
+// src/config/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
@@ -7,10 +7,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyDP6XNlI1jUgHN3pVWIXNZjGT3YWXKSdes",
   authDomain: "gweenlearn.firebaseapp.com",
   projectId: "gweenlearn",
-  storageBucket: "gweenlearn.firebasestorage.app",
+  storageBucket: "gweenlearn.appspot.com",
   messagingSenderId: "915816429541",
   appId: "1:915816429541:web:65c885efda4472930c210c",
-  measurementId: "G-RSGBWRE6BD",
+  measurementId: "G-RSGBWRE6BD"
 };
 
 const app = initializeApp(firebaseConfig);
