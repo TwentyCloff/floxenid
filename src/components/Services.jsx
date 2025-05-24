3d34321b2d2b36ee9b2c01b9e14155942bdc14bb
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-24 23:28:02 +0700

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,161 +1,167 @@
-import { useState } from "react";
-import { service1, service2, service3, check } from "../assets";
-import { brainwaveServices, brainwaveServicesIcons } from "../constants";
-import Generating from "./Generating";
-import Heading from "./Heading";
+import { PropsWithChildren, useRef, useState } from "react";
+import { TiLocationArrow } from "react-icons/ti";
 import Section from "./Section";
-import { curve } from "../assets";
-import {
-  Gradient,
-  PhotoChatMessage,
-  VideoBar,
-  VideoChatMessage,
-} from "./design/Services";
 
-const Services = () => {
-  const [isPlaying, setIsPlaying] = useState(true);
+interface BentoTiltProps {
+  className?: string;
+}
+
+const BentoTilt = ({
+  children,
+  className = "",
+}: PropsWithChildren<BentoTiltProps>) => {
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect();
+
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.5) * 5;
+    const tiltY = (relativeX - 0.5) * -5;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`;
+
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformStyle("");
+  };
 
   return (
+    <div
+      ref={itemRef}
+      className={className}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
+      {children}
+    </div>
+  );
+};
+
+interface BentoCardProps {
+  src: string;
+  title: React.ReactNode;
+  description?: string;
+}
+
+const BentoCard = ({ src, title, description }: BentoCardProps) => {
+  return (
+    <article className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
+
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+        <div>
+          <h1 className="bento-title special-font">{title}</h1>
+          {description && (
+            <p className="tetx-xl mt-3 max-w-64 md:text-base">{description}</p>
+          )}
+        </div>
+      </div>
+    </article>
+  );
+};
+
+const Services = () => {
+  return (
     <Section id="how-to-use">
-      <div className="container">
-        <Heading
-          title={
-            <>
-              Generative AI made for{" "}
-              <span className="inline-block relative font-semibold">
-                creators
-                <img
-                  src={curve}
-                  className="absolute top-full left-0 w-full xl:-mt-2 pointer-events-none select-none"
-                  width={624}
-                  height={28}
-                  alt="Curve"
-                />
-              </span>
-            </>
-          }
-          text="Brainwave unlocks the potential of AI-powered applications."
-        />
+      <div className="bg-black pb-52">
+        <div className="container mx-auto px-3 md:px-10">
+          <div className="px-5 py-32">
+            <p className="font-circular-web text-lg text-blue-50">
+              Into the Metagame Layer
+            </p>
 
-        <div className="relative">
-          {/* Service 1 */}
-          <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
-            <div className="absolute top-0 left-0 w-full h-full pointer-events-none select-none md:w-3/5 xl:w-auto">
-              <img
-                className="w-full h-full object-cover md:object-right"
-                width={800}
-                height={730}
-                alt="Smartest AI"
-                src={service1}
+            <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+              Immerse yourself in a rich and ever-expanding universe where a
+              vibrant array of products converge into an interconnected overlay
+              experience on your world.
+            </p>
+          </div>
+
+          <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+            <BentoCard
+              src="/videos/feature-1.mp4"
+              title={
+                <>
+                  radia<b>n</b>t
+                </>
+              }
+              description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
+            />
+          </BentoTilt>
+
+          <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+            <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+              <BentoCard
+                src="/videos/feature-2.mp4"
+                title={
+                  <>
+                    zig<b>m</b>a
+                  </>
+                }
+                description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
               />
-            </div>
+            </BentoTilt>
 
-            <div className="relative z-1 max-w-[17rem] ml-auto">
-              <h4 className="h4 mb-4">Smartest AI</h4>
-              <p className="body-2 mb-[3rem] text-n-3">
-                Brainwave unlocks the potential of AI-powered applications.
-              </p>
-              <ul className="body-2">
-                {brainwaveServices.map((item, i) => (
-                  <li
-                    key={i}
-                    className="flex items-start py-4 border-t border-n-6"
-                  >
-                    <img
-                      width={24}
-                      height={24}
-                      src={check}
-                      alt="check"
-                      className="pointer-events-none select-none"
-                    />
-                    <p className="ml-4">{item}</p>
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+              <BentoCard
+                src="/videos/feature-3.mp4"
+                title={
+                  <>
+                    n<b>e</b>xus
+                  </>
+                }
+                description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."
+              />
+            </BentoTilt>
 
-            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg:right-auto lg:bottom-8 lg:-translate-x-1/2" />
+            <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+              <BentoCard
+                src="/videos/feature-4.mp4"
+                title={
+                  <>
+                    az<b>u</b>l
+                  </>
+                }
+                description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
+              />
+            </BentoTilt>
+
+            <BentoTilt className="bento-tilt_2">
+              <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+                <h1 className="bento-title special-font max-w-64 text-black">
+                  M<b>o</b>re co<b>m</b>ing so<b>o</b>n!
+                </h1>
+
+                <TiLocationArrow className="m-5 scale-[5] self-end" />
+              </div>
+            </BentoTilt>
+
+            <BentoTilt className="bento-tilt_2">
+              <video
+                src="/videos/feature-5.mp4"
+                loop
+                muted
+                autoPlay
+                className="size-full object-cover object-center"
+              />
+            </BentoTilt>
           </div>
-
-          {/* Service 2 & 3 */}
-          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
-            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
-              <div className="absolute inset-0">
-                <img
-                  src={service2}
-                  alt="Robot"
-                  className="h-full w-full object-cover pointer-events-none select-none"
-                  width={630}
-                  height={750}
-                />
-              </div>
-
-              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
-                <h4 className="h4 mb-4">Photo editing</h4>
-                <p className="body-2 mb-[3rem] text-n-3">
-                  Automatically enhance your photos using our AI app&apos;s
-                  photo editing feature. Try it now!
-                </p>
-              </div>
-
-              <PhotoChatMessage />
-            </div>
-
-            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
-              <div className="py-12 px-4 xl:px-8">
-                <h4 className="h4 mb-4">Video generation</h4>
-                <p className="body-2 mb-[2rem] text-n-3">
-                  The world&apos;s most powerful AI photo and video art
-                  generation engine. What will you create?
-                </p>
-
-                <ul className="flex items-center justify-between">
-                  {brainwaveServicesIcons.map((icon, i) => (
-                    <li
-                      key={i}
-                      className={`flex items-center justify-center rounded-2xl ${
-                        i === 2
-                          ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]"
-                          : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
-                      }`}
-                    >
-                      <div
-                        className={
-                          i === 2
-                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
-                            : ""
-                        }
-                      >
-                        <img
-                          src={icon}
-                          width={24}
-                          height={24}
-                          alt={`icon-${i}`}
-                        />
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-
-              <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
-                <img
-                  src={service3}
-                  className={`w-full h-full object-cover ${
-                    isPlaying && "animate-pulse"
-                  } pointer-events-none select-none`}
-                  width={520}
-                  height={400}
-                  alt="Scary Robot"
-                />
-
-                <VideoChatMessage isPlaying={isPlaying} />
-                <VideoBar isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
-              </div>
-            </div>
-          </div>
-
-          <Gradient />
         </div>
       </div>
     </Section>
