3c18431112a875360cc5349d133b3856a84fff9d
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-29 13:48:38 +0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -48,17 +48,22 @@ export const navigation = [
     url: "#hero",
   },
   {
+    id: "1",
+    title: "Features",
+    url: "#features",
+  },
+  {
     id: "2",
     title: "How To Use",
     url: "#how-to-use",
   },
   {
-    id: "1",
+    id: "3",
     title: "Pricing",
     url: "#pricing",
   },
   {
-    id: "3",
+    id: "4",
     title: "Roadmap",
     url: "#roadmap",
   },
