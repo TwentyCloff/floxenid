33a84932e3400bca2113a6a4a0edae758c489f43
TwentyCloff <twencloff@gmail.com>
2025-06-23 16:21:32 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import Nav1 from "./components/Nav1";
 
 const Home = () => (
   <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
     <Header />
     <Hero />
     <Footer />
+    <Nav1 />
   </div>
 );
 
