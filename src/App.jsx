a41235645abdbf792eb17486f3f0dad5d650ae26
TwentyCloff <twencloff@gmail.com>
2025-06-25 10:38:38 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Hero1 from "./components/Hero1";
 import Clb from "./components/collab";
 import Footer from "./components/Footer";
 import Nav1 from "./components/Nav1";
-import Log from "./components/Auth";
+import Auth from "./components/Auth"; // Changed from Log to Auth
 import Profile from "./components/Profile";
-
 
 const Home = () => (
   <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
@@ -71,12 +70,14 @@ const NotFound = () => (
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-      <Route path="/Profile" element={<Profile />} />
-      <Route path="/signup" element={<Auth />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<Auth />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
