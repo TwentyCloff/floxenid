70822362042ba1fce2e3baaab8a92f92af841b15
TwentyCloff <twencloff@gmail.com>
2025-06-27 13:19:14 +0700

    Update ButtonTest.jsx

--- src/components/ButtonTest.jsx
+++ src/components/ButtonTest.jsx
@@ -1,7 +1,5 @@
-import { cn } from "@/lib/utils";
-import { ElementType, ComponentPropsWithoutRef } from "react";
+import { ElementType } from "react";
 
-// StarBorderDemo component
 export function StarBorderDemo() {
   return (
     <div className="space-y-8">
@@ -14,23 +12,26 @@ export function StarBorderDemo() {
 
 export function StarBorder({
   as: Component = 'button',
-  className,
-  color = '#ffffff', // White animation color
+  className = '',
+  color = '#ffffff', // White animation
   speed = "6s",
   children,
   ...props
 }) {
+  // Simple class merging utility replacement
+  const mergeClasses = (...classes) => classes.filter(Boolean).join(' ');
+
   return (
     <Component 
-      className={cn(
+      className={mergeClasses(
         "relative inline-block py-[1px] overflow-hidden rounded-[20px]",
         className
       )} 
       {...props}
     >
-      {/* Bottom animation layer */}
+      {/* Bottom animation (white) */}
       <div
-        className={cn(
+        className={mergeClasses(
           "absolute w-[300%] h-[50%] bottom-[-11px] right-[-250%] rounded-full animate-star-movement-bottom z-0",
           "opacity-20 dark:opacity-70" 
         )}
@@ -40,9 +41,9 @@ export function StarBorder({
         }}
       />
       
-      {/* Top animation layer */}
+      {/* Top animation (white) */}
       <div
-        className={cn(
+        className={mergeClasses(
           "absolute w-[300%] h-[50%] top-[-10px] left-[-250%] rounded-full animate-star-movement-top z-0",
           "opacity-20 dark:opacity-70"
         )}
@@ -52,8 +53,8 @@ export function StarBorder({
         }}
       />
       
-      {/* Content container with black background */}
-      <div className={cn(
+      {/* Black button container */}
+      <div className={mergeClasses(
         "relative z-1 border text-foreground text-center text-base py-4 px-6 rounded-[20px]",
         "bg-black border-black/40",
         "dark:bg-black dark:border-black"
